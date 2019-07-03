a5b81f7aec053f8e35b105fd81ce46cbc3c59aa4
Will Worth <will.worth@gmail.com>
2019-07-03 13:56:13 +0200

    nesting testing!

--- script.test.js
+++ script.test.js
@@ -6,18 +6,31 @@ dbMock = [
   "morecats.com",
   "otherdogs.com",
   "myfavouritememesite.com",
-  "elpais.com"
+  "elpais.com",
+  "bigdogs.com",
+  "weirddogs.com"
 ];
 
-it("explanatory string", () => {
-  console.log("running");
-  //   console.log(googleSearch("cats", dbMock));
-});
+describe("googleSearch", () => {
+  it("explanatory string", () => {
+    console.log("running");
+    //   console.log(googleSearch("cats", dbMock));
+  });
 
-// matchers:
-// https://jestjs.io/docs/en/using-matchers
+  // matchers:
+  // https://jestjs.io/docs/en/using-matchers
 
-it("is searching", () => {
-  expect(googleSearch("testtest", dbMock)).toEqual([]);
-  expect(googleSearch("dogs", dbMock)).toEqual(["dogs.com", "otherdogs.com"]);
+  it("is searching", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]);
+    //   expect(googleSearch("dogs", dbMock)).toEqual(["dogs.com", "otherdogs.com"]);
+  });
+
+  it("works with undefined and null", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]),
+      expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it("doesn't return more than 3 matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
+  });
 });
